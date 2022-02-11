0582bd0591e5a943139085fb4437586dc6024c3d
mizupi <mizukif2206@gmail.com>
2022-02-11 12:32:27 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,20 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :days
 
+  validates :name, presence: true
+  validates :description, presence: true
+
+  with_options presence: true, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' } do
+    validates :price
+  end
+
+  validates :price, numericality: { in: 300..9_999_999, message: 'is out of setting range' }
+
+  with_options presence: true, numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :cost_id
+    validates :prefecture_id
+    validates :days_id
+  end
 end
