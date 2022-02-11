ab841ac84a808f75dc298a8c1dd5cdf1c2b70d47
mizupi <mizukif2206@gmail.com>
2022-02-11 11:46:33 +0900

    activehashモデルの作成とアソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
-  
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :cost
+  belongs_to :prefecture
+  belongs_to :days
+
 end
