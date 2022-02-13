03d7003e6d77f37e154f108d5fb2dd25ec392e2d
mizupi <mizukif2206@gmail.com>
2022-02-13 15:57:34 +0900

    出品者以外の画面遷移制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items  = Item.includes(:user).order('created_at DESC')
@@ -47,4 +48,8 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def move_to_index
+    @item = Item.find(params[:id])
+    redirect_to action: :index unless @item.user == current_user
+  end
 end
