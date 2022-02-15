3fa20b55d3e464179e0c19000810410213fb5a10
mizupi <mizukif2206@gmail.com>
2022-02-15 18:36:08 +0900

    バリデーションの修正

--- app/models/record_destination.rb
+++ app/models/record_destination.rb
@@ -8,10 +8,11 @@ class RecordDestination
     validates :user_id, :item_id
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :city, :address
-    validates :phone_number, length: { maximum: 11 },
-                             numericality: { only_integer: true, message: 'is invalid. Input only number' }
+    validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input only number' }
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  validates :phone_number, length: { minimum: 10, maximum: 11, ,essage: "is too short" }
 
   def save
     purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
