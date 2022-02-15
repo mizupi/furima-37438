cdae110ce4afa481c2ec8f7d90351904aaa76bee
mizupi <mizukif2206@gmail.com>
2022-02-15 17:27:46 +0900

    クレジットカード決済機能の設定

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,12 +1,14 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
     @record_destination = RecordDestination.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @record_destination = RecordDestination.new(purchase_record_params)
     if @record_destination.valid?
+      pay_item
       @record_destination.save
       redirect_to root_path
     else
@@ -16,7 +18,22 @@ class PurchaseRecordsController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def purchase_record_params
-    params.require(:record_destination).permit(:post_code, :prefectute, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:record_destination).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_record_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/record_destination.rb
+++ app/models/record_destination.rb
@@ -1,18 +1,21 @@
 class RecordDestination
-
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefectute, :city, :address, :building_name, :phone_number, :purchase_record_id
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number,
+                :purchase_record_id, :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :purchase_record_id
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :token
+    validates :user_id, :item_id
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :city, :address
-    validates :phone_number, length: { maximum: 11 }, numericality: { only_integer: true,  message: "is invalid. Input only number"}
+    validates :phone_number, length: { maximum: 11 },
+                             numericality: { only_integer: true, message: 'is invalid. Input only number' }
   end
-  validates :prefectute, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
-    purchase_record = purchaseRecord.create(user_id: user_id, item_id: item_id)
-    Destination.create(post_code: post_code, prefecture: prefectute, city: city, address: address, building_name: building_name, phone_number: phone_number, purchase_record_id: purchase_record_id)
+    purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
+    Destination.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address,
+                       building_name: building_name, phone_number: phone_number, purchase_record_id: purchase_record_id)
   end
 end
