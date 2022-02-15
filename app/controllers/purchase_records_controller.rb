20579189c27c98292f5b5e773eb9039174f3490f
mizupi <mizukif2206@gmail.com>
2022-02-15 12:39:16 +0900

    購入記録コントローラーの作成

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,2 +1,22 @@
 class PurchaseRecordsController < ApplicationController
+  def index
+    @record_destination = RecordDestination.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @record_destination = RecordDestination.new(purchase_record_params)
+    if @record_destination.valid?
+      @record_destination.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_record_params
+    paramas.require(:record_destination).permit(:post_code, :prefectute, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: paramas[:item_id])
+  end
 end
